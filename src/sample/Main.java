3a1c884c55f124f939df587bb1b755892146423d
Lukienko <sergo.net@mail.ru>
2020-12-21 20:24:51 +0300

    final commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,38 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
+    Scene client;
+    Scene registrartion;
+    Stage window;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
+        primaryStage.setTitle("Home Chat");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() { //при закрытие окна, программа закрывается, но мы это сделали через демона
+            @Override
+            public void handle(WindowEvent event) {
+
+            }
+        });
     }
 
 
     public static void main(String[] args) {
+        // метод из класса Application и нужен для определения что это javaFX
         launch(args);
     }
+
+
 }
